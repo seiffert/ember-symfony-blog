7bc3cd897cba0a2edebafac587bba3b2d4116ec3
Paul Seiffert <paul.seiffert@gmail.com>
2014-10-06 23:25:31 +0200

    Renamed test blog posts

--- api/src/Seiffert/Bundle/BlogApiBundle/DataFixtures/Orm/BlogPosts.php
+++ api/src/Seiffert/Bundle/BlogApiBundle/DataFixtures/Orm/BlogPosts.php
@@ -15,7 +15,7 @@ class BlogPosts extends AbstractFixture
     public function load(ObjectManager $manager)
     {
         $post = new BlogPost(
-            'Tag 1',
+            'Post 1',
             'first-post',
             new \DateTime(),
             'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
@@ -41,7 +41,7 @@ class BlogPosts extends AbstractFixture
         $manager->persist($post);
 
         $post = new BlogPost(
-            'Tag 2',
+            'Post 2',
             'second-post',
             new \DateTime(),
             'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
